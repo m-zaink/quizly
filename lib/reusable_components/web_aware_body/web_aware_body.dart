602f77810ffd9cbb4ed8f6521ce254eadade4f50
m-zaink <sidsmith9@gmail.com>
2020-05-25 13:19:34 +0530

    Update WebAwareBody with better breakpoints.

--- lib/reusable_components/web_aware_body/web_aware_body.dart
+++ lib/reusable_components/web_aware_body/web_aware_body.dart
@@ -17,9 +17,22 @@ class WebAwareBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: FractionallySizedBox(
-        widthFactor: kIsWeb ? 0.6 : 1.0,
+        widthFactor: widthFactor(context),
         child: child,
       ),
     );
+  }
+
+  double widthFactor(BuildContext context) {
+    if (!kIsWeb) return 1.0;
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth > 1920)
+      return 0.3;
+    else if (screenWidth > 1024)
+      return 0.5;
+    else if (screenWidth > 720)
+      return 0.7;
+    else
+      return 0.9;
   }
 }
