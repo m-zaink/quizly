676154350cdc95f3e71fc9d52e6d7ab6452a957e
m-zaink <sidsmith9@gmail.com>
2020-05-25 20:48:20 +0530

    Update HomeScreen and MaterialApp in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quizly/models/session.dart';
 import 'package:quizly/screens/home_screen/home_screen.dart';
+import 'package:uuid/uuid.dart';
 
 void main() => runApp(QuizlyApp());
 
 class QuizlyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: theme,
-      home: HomeScreen(),
+    return Provider<Session>(
+      create: (context) => Session(token: Uuid().v1()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme,
+        home: HomeScreen(),
+      ),
     );
   }
 
   ThemeData get theme => ThemeData(
-    primarySwatch: Colors.indigo,
-    scaffoldBackgroundColor: Colors.white,
-    appBarTheme: AppBarTheme(
-      textTheme: TextTheme(),
-    ),
-    buttonTheme: ButtonThemeData(
-      textTheme: ButtonTextTheme.primary,
-    ),
-  );
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(),
+        ),
+        buttonTheme: ButtonThemeData(
+          textTheme: ButtonTextTheme.primary,
+        ),
+      );
 }
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -4,9 +4,13 @@
 
 import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:quizly/models/quiz_settings.dart';
+import 'package:quizly/models/session.dart';
 import 'package:quizly/reusable_components/choice_screen/choice_screen.dart';
 import 'package:quizly/reusable_components/web_aware_body/web_aware_body.dart';
+import 'package:quizly/screens/quiz_screen/quiz_bloc/bloc.dart';
 import 'package:quizly/screens/quiz_screen/quiz_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -82,6 +86,11 @@ class _HomeScreenState extends State<HomeScreen> {
           labelText: 'Quiz for how many questions?',
           counterText: '1 - 50',
         ),
+        onChanged: (value) {
+          if (value.isValidNumberOfQuestions) {
+            quizSettings.numberOfQuestions = int.parse(value);
+          }
+        },
         validator: (input) {
           if (input.isEmpty || input.isNotValidNumberOfQuestions)
             return 'Please enter a valid number of questions';
